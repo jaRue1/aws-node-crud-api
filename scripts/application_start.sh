d9a1bf456e4839700d3576f6592b85ef60cca987
jaRue1 <jaruejohnson@outlook.com>
2022-10-04 17:46:08 -0400

    updated scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 777 /home/ec2-user/aws-node-crud-api
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/aws-node-crud-api
+cd /home/my-api/aws-node-crud-api
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-nodemon app.js > app.out.log 2> app.err.log < /dev/null & 
+node app.js > app.out.log 2> app.err.log < /dev/null & 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/aws-node-crud-api"
+DIR="/home/my-api/aws-node-crud-api"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
