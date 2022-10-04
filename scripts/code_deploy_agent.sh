b87458441e7ba298d0ef3319064d81fdc9c27994
jaRue1 <jaruejohnson@outlook.com>
2022-10-03 22:03:47 -0400

    added a script

---
+++ scripts/code_deploy_agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
